963fa10c27736a669665c70694beef58d573f06c
CoolCactuz <CoolCactuz@gitlab.com>
2018-12-11 15:57:12 +0100

    settato maven e collegato travisCI, close#1 close#2 close#3 close#4

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# TOS-assignment2
+# TOS-assignment2 <br>
+[![Build Status](https://travis-ci.org/Coolcactuz/TOS-assignment2.svg?branch=master)](https://travis-ci.org/Coolcactuz/TOS-assignment2)
---
+++ restaurant-manager/src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// [LUCA] [STOCCO] [1125280]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
